20d6e702c0e35aeb6518ed1e3594131db3fc6aca
Ricardo-rtc <ricardotamahecarvalho@gmail.com>
2021-08-09 17:54:15 -0300

    Att

---
+++ SQL/HROADS-DDL.sql
@@ -0,0 +1,41 @@
+CREATE DATABASE SENAI_HROADS_MANHA;
+GO
+
+USE SENAI_HROADS_MANHA;
+GO
+
+CREATE TABLE Tipo_Habilidade(
+	idTipoHabilidade INT PRIMARY KEY IDENTITY,
+	nomeTipoHabilidade VARCHAR(50),
+);
+GO
+
+CREATE TABLE Classe(
+	idClasse INT PRIMARY KEY IDENTITY,
+	nomeClasse VARCHAR(50),
+); 
+GO
+
+CREATE TABLE Personagem(
+	idPersonagem INT PRIMARY KEY IDENTITY,
+	idClasse INT FOREIGN KEY REFERENCES Classe(idClasse),
+	nome VARCHAR(50),
+	capMana TINYINT,
+	capVida TINYINT,
+	dataAtt DATETIME,
+	dataCriacao DATETIME
+);
+GO
+
+CREATE TABLE Habilidade(
+	idHabilidade INT PRIMARY KEY IDENTITY,
+	idTipoHabilidade INT FOREIGN KEY REFERENCES Tipo_Habilidade(idTipoHabilidade),
+);
+GO
+
+CREATE TABLE Classe_Habilidade(
+	idClasseHabilidade INT PRIMARY KEY IDENTITY,
+	idClasse INT FOREIGN KEY REFERENCES Classe(idClasse),
+	idHabilidade INT FOREIGN KEY REFERENCES Habilidade(idHabilidade),
+);
+GO
